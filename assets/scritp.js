c42a54e080a25888a67614586362bb31f5e069b6
serdarabdulkadir <serdarabdulkadir04@gmail.com>
2022-05-22 22:57:40 +0300

    first commit

---
+++ assets/scritp.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', 
+{
+    // Optional parameters
+    direction: 'horizantal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  });
